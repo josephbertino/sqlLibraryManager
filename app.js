72cd227dd31ea01a0c8841e4512ccf56225457a6
Joe Bertino <joseph.bertino@gmail.com>
2021-08-26 19:20:45 -0600

    adding routes now

--- app.js
+++ app.js
@@ -9,9 +9,24 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var { sequelize } = require('./models');
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Authentication to the sequelize database successful!");
+
+    await sequelize.sync({force: true});
+    console.log("Model synced to the database!")
+
+  } catch (error) {
+    throw error;
+  }
+})();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,8 +38,14 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use( (req, res, next) => {
+  const error = new Error();
+  error.status = 404;
+  error.message = "Sorry! We couldn't find the page you were looking for."
+  res.status(404).render('page-not-found', { 
+    error, 
+    title: "Page Not Found"
+  });
 });
 
 // error handler
@@ -34,8 +55,14 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  err.status = err.status || 500;
+  err.message = err.message || "500 Error. Apologies!"
+  console.log(err.status + ": " + err.message);
+
+  res.status(err.status).render('error', {
+    error: err, 
+    title: "Server Error"
+  });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var { Book } = require("../models");
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Home route should redirect to the /books route
+router.get('/', asyncHandler(async (req, res) => {
+  // res.render('index', { title: 'Express' });
+  res.redirect('/books');
+}));
+
+// Show the full list of books
+router.get('/books', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  res.render('index', {
+    books,
+    title: "Books",
+  })
+}));
 
 module.exports = router;
